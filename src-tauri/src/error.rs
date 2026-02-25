01298884b03e48b1090ef293720a7222c102dd33
Michelle Mayes <michellep1994@gmail.com>
2026-02-24 22:10:48 -0600

    scaffold: organize Rust backend modules

---
+++ src-tauri/src/error.rs
@@ -0,0 +1,27 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum NootleError {
+    #[error("Database error: {0}")]
+    Database(#[from] rusqlite::Error),
+
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Serialization error: {0}")]
+    Serde(#[from] serde_json::Error),
+
+    #[error("{0}")]
+    Other(String),
+}
+
+impl serde::Serialize for NootleError {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+pub type Result<T> = std::result::Result<T, NootleError>;
